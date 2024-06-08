52e51c19f107e5e1d586b0cedb13ab388602185e
Anca <a.e.strulea@gmail.com>
2024-06-08 15:48:41 +0200

    add: display path works

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request
-import subprocess
+import geojson 
+from shapely.geometry import LineString, Point
+import graph
 
 app = Flask(__name__, static_folder='', static_url_path='')
+G = graph.create_graph('simplified_transport.json')
 
 @app.route('/')
 def index():
@@ -9,20 +12,17 @@ def index():
 
 @app.route('/find-path', methods=['POST'])
 def find_path():
-    
     start_lat = request.json.get('start_lat')
     start_lng = request.json.get('start_lng')
     end_lat = request.json.get('end_lat')
     end_lng = request.json.get('end_lng')
 
-    print('Starting point:', start_lat, start_lng)
-    print('Ending point:', end_lat, end_lng)
+    start = graph.get_closest_node(G, (start_lng, start_lat))
+    end = graph.get_closest_node(G, (end_lng, end_lat))
 
-    # subprocess.run(['python3', 'script.py'])
-    subprocess.run(['python3', 'script.py', str(start_lat), str(start_lng), str(end_lat), str(end_lng)])
-    
-    # Return a response
-    return jsonify({'message': 'Python script executed successfully'})
+    path = graph.find_shortest_path(G, start, end)
+
+    return jsonify(geojson.Feature(geometry=LineString(path)))
 
 if __name__ == '__main__':
     app.run(debug=True)
